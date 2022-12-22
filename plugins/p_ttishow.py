e2c09e2d415bf068bb7a61b8d4fb22942459762f
Vihara Senindu <110663030+viharasenindu@users.noreply.github.com>
2022-12-22 21:48:50 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/SECLK')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/SinhalaCryptoNews')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
